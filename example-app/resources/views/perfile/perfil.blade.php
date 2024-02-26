338827849aa9622c0619270eb858afc5b5ce016e
BJaviersito <losthyghost@gmail.com>
2024-02-26 08:19:34 -0600

    Haciendo el html

---
+++ example-app/resources/views/perfile/perfil.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/netflix-icon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/user.css') }}">
+
+
+    <title>Perfiles</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <h1>¿Quién está viendo ahora?</h1>
+
+        <div class="card-users">
+            <div class="card">
+                <a href="home-user.html">
+                    <img src="img/rei.jpg">
+                    <p>Walter</p>
+                </a>
+            </div>
+
+            <div class="card-users2">
+                <div class="card">
+                    <a href="home-user.html">
+                        <img src="img/5fb80e34333482d9548ba960f70f5926.jpg">
+                        <p>Javier</p>
+                    </a>
+                </div>
+
+            <div class="card">
+                <a href="">
+                    <img src="img/perfil02.png">
+                    Infantil
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="">
+                    <img src="img/perfil03.png">
+                    Agregar perfil
+                </a>
+            </div>
+        </div>
+
+        <a href="">Generar perfil</a>
+    </div>
+</body>
+
+</html>
